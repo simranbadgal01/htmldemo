470a5a98464a02450069f70c033ee55c60ce70af
simran <you@example.com>
2024-03-03 19:21:38 -0800

    tasks list

---
+++ index.js
@@ -0,0 +1,56 @@
+var price=23;
+console.log(price);
+
+
+var myName='simran';
+console.log(myName);
+
+
+var myName="Simran";
+console.log(myName);
+
+
+var price=15000;
+console.log(`lsvins bought a course at ${price}`);
+
+var num="20"+5;
+console.log(num);
+
+var num="20"+5;
+console.log(typeof (num));
+
+
+var num=1/10;
+console.log(num);
+
+var num="Hello"-(d3/2);
+console.log(num);
+
+var myCourse=1500;
+  myCourse=2000;
+console.log(myCourse);
+
+function myCoursefun(){
+    var myCourse=2000;
+    console.log(myCourse);
+}
+myCoursefun();
+
+
+let myCourse=28000;
+console.log(myCourse);
+
+let myCourse=2800;
+myCourse=1500;
+console.log(myCourse);
+
+let myCourse=2800;
+let myCourse=1500;
+console.log(myCourse);
+
+let uservar=prompt();
+console.log(uservar);
+VM2552:2 null
+undefined
+let uservalue=prompt("enter your current salary");
+console.log(uservalue);
